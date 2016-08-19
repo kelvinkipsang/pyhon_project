285c1e333db0752a7a4dba52faa59d3cf6435a07
kelvinkipsang <kelvinkipsang@gmail.com>
2016-08-19 16:24:14 +0300

    created tables

--- app.py
+++ app.py
@@ -6,6 +6,22 @@ app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/testdb'
 db=SQLAlchemy(app)
 
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(20), unique=True)
+    sname = db.Column(db.String(20), unique=True)
+
+    def __init__(self, fname, sname):
+        self.fname = fname
+        self.sname = sname
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+
+
+
 @app.route('/')
 def index():
     return "hello world"
